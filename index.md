915fe152bc9af95414ae0f88bd5f794372b9608e
sakikuroe <sakikuroe96@gmail.com>
2022-01-15 14:39:29 +0900

    Initial commit

---
+++ index.md
@@ -0,0 +1,11 @@
+---
+documentclass: ltjsarticle
+title: Primality test
+author: Saki KUROE
+---
+
+<input type="text" id="io">
+<button> Determine </button>
+
+## Please input an integer value.
+<script src="scripts/main.js"></script>
---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h2');
+
+function is_prime(n) {
+    if (n <= 1) {
+        return false;
+    }
+
+    let i = 2;
+    while (i * i <= n) {
+        if (n % i == 0) {
+            return false;
+        }
+        i++;
+    }
+    return true;
+}
+
+function determine_prime() {
+    let number = document.getElementById("io").value;
+    if (isNaN(number)) {
+        myHeading.textContent = 'Please input an integer value.';
+    } else if (is_prime(number)) {
+        myHeading.textContent = number + ' is prime number.';
+    } else {
+        myHeading.textContent = number + ' is not prime number.';
+    }
+}
+
+myButton.onclick = function() {
+    determine_prime();
+}
