893fa3a7bda3935a0248c9036b2a0a2895214f0e
sakikuroe <sakikuroe96@gmail.com>
2022-01-15 15:12:26 +0900

    Update to show integer factorization

--- scripts/main.js
+++ scripts/main.js
@@ -16,6 +16,35 @@ function is_prime(n) {
     return true;
 }
 
+function factorization(n) {
+    let res = '';
+    let i = 2;
+    let f = true;
+    while (i * i <= n) {
+        while (n % i == 0) {
+            if (f) {
+                f = false;
+                res = res + i;
+            } else {
+                res = res + ' * ' + i;
+            }
+            n /= i;
+        }
+        i += 1;
+    }
+
+    if (n != 0 && n != 1) {
+        if (f) {
+            f = false;
+            res = res + n;
+        } else {
+            res = res + ' * ' + n;
+        }
+    }
+
+    return res;
+}
+
 function determine_prime() {
     let number = document.getElementById("io").value;
     if (isNaN(number)) {
@@ -23,7 +52,8 @@ function determine_prime() {
     } else if (is_prime(number)) {
         myHeading.textContent = number + ' is a prime number.';
     } else {
-        myHeading.textContent = number + ' is not a prime number.';
+        myHeading.textContent = number + ' is not a prime number. '
+        + number + ' = ' + factorization(number);
     }
 }
 
